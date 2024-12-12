ef3bd7c2e405fd74eb5646f994ec8a01da2f3fb0
Tapoban Ray <raytapoban@gmail.com>
2024-12-12 17:12:08 +0530

    Updated global README

---
+++ README.md
@@ -0,0 +1,7 @@
+# Important Flutter tools
+
+#### This repository contains all the important flutter tools that I have learnt so far.
+
+## Here are all the codes and notes of each lesson
+- ### [Learn how to use Chopper Package in Flutter](chopper_tutorial)
+- ### [Learn how to generate data classes using freezed](freezed_package_tutorial)
---
+++ chopper_tutorial/lib/data/post_api_service.chopper.dart
@@ -0,0 +1,55 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'post_api_service.dart';
+
+// **************************************************************************
+// ChopperGenerator
+// **************************************************************************
+
+// coverage:ignore-file
+// ignore_for_file: type=lint
+final class _$PostApiService extends PostApiService {
+  _$PostApiService([ChopperClient? client]) {
+    if (client == null) return;
+    this.client = client;
+  }
+
+  @override
+  final Type definitionType = PostApiService;
+
+  @override
+  Future<Response<dynamic>> getPosts() {
+    final Uri $url = Uri.parse('https://jsonplaceholder.typicode.com/posts');
+    final Request $request = Request(
+      'GET',
+      $url,
+      client.baseUrl,
+    );
+    return client.send<dynamic, dynamic>($request);
+  }
+
+  @override
+  Future<Response<dynamic>> getPost(int id) {
+    final Uri $url =
+        Uri.parse('https://jsonplaceholder.typicode.com/posts/${id}');
+    final Request $request = Request(
+      'GET',
+      $url,
+      client.baseUrl,
+    );
+    return client.send<dynamic, dynamic>($request);
+  }
+
+  @override
+  Future<Response<dynamic>> postPost(Map<String, dynamic> body) {
+    final Uri $url = Uri.parse('https://jsonplaceholder.typicode.com/posts');
+    final $body = body;
+    final Request $request = Request(
+      'POST',
+      $url,
+      client.baseUrl,
+      body: $body,
+    );
+    return client.send<dynamic, dynamic>($request);
+  }
+}
